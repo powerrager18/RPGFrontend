0b1c354dd406f1e4e231162fca51dda921cae092
Pedro Henrique Moreira <moreirapedro1703@gmail.com>
2022-07-15 20:06:32 -0300

    RPG Frontend 1.0

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -21,7 +21,7 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -49,7 +49,10 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
-  }
+  }else {
+    console.log("Não conseguiu registrar o service worker. NODE_ENV: ",
+    process.env.NODE_ENV);
+    }
 }
 
 function registerValidSW(swUrl, config) {
